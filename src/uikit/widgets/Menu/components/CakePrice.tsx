08bbf5cca71383d33fb0f3acd28721eeb4cdbc7c
Beyond-20 <beyond8620@gmail.com>
2021-09-10 23:06:59 +0800

    Change monster price icon

--- src/uikit/widgets/Menu/components/CakePrice.tsx
+++ src/uikit/widgets/Menu/components/CakePrice.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { MonsterLogoIcon } from 'uikit'
 import { PancakeRoundIcon } from '../../../components/Svg'
 import Text from '../../../components/Text/Text'
 import Skeleton from '../../../components/Skeleton/Skeleton'
@@ -27,7 +28,8 @@ const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
       href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
       target="_blank"
     >
-      <PancakeRoundIcon width="24px" mr="8px" />
+      <MonsterLogoIcon width="40px" />
+      {/* <PancakeRoundIcon width="24px" mr="8px" /> */}
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
   ) : (
