f0443d903228401de67fdea99375d7740dcb97ba
slarkjm <slarkjm@outlook.com>
2021-09-22 10:11:51 +0800

    fixed background color

--- src/views/Pools/components/PoolsTable/ActionPanel/ActionPanel.tsx
+++ src/views/Pools/components/PoolsTable/ActionPanel/ActionPanel.tsx
@@ -110,6 +110,7 @@ const Container = styled.div<{ expanded }>`
   width: 100%;
   flex-direction: column-reverse;
   padding: 24px;
+  background-color: #EAF2F7;
 
   ${({ theme }) => theme.mediaQueries.lg} {
     flex-direction: row;
